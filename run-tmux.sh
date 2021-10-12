d50b9a4dbccd6f1298987c7d3e89f522f334d809
Robert Bohne <robert.bohne@redhat.com>
2021-10-12 17:16:40 +0200

    Update run-tmux.sh

--- run-tmux.sh
+++ run-tmux.sh
@@ -12,14 +12,14 @@ SERVER=$1
 
 if [ -z $TMUX ] ; then
   echo "Start new tmux session"
-  tmux new-session -d -s demo-$PID "watch curl -s -k https://homer-web-simpson.${SERVER}/check"
+  tmux new-session -d -s demo-$PID "watch curl -s -k https://homer-simpson.${SERVER}/demo"
 fi
-tmux split-window -d -t 0 -v  "watch curl -s -k https://marge-web-simpson.${SERVER}/check"
-tmux split-window -d -t 0 -h  "watch curl -s -k https://selma-web-bouvier.${SERVER}/check"
-tmux split-window -d -t 2 -h  "watch curl -s -k https://patty-web-bouvier.${SERVER}/check"
+tmux split-window -d -t 0 -v  "watch curl -s -k https://marge-simpson.${SERVER}/demo"
+tmux split-window -d -t 0 -h  "watch curl -s -k https://selma-bouvier.${SERVER}/demo"
+tmux split-window -d -t 2 -h  "watch curl -s -k https://patty-bouvier.${SERVER}/demo"
 if [ -z $TMUX ] ; then
   echo "Attach to session"
   tmux attach-session -t demo-$PID
 else
-  watch curl -s -k http://homer-simpson.${SERVER}/check
+  watch curl -s -k http://homer-simpson.${SERVER}/demo
 fi
